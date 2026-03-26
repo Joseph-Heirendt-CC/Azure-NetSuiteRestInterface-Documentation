642199bbbfc4704e886d6ece82ad623f4af351a3
Joseph Heirendt <jheirendt@citrincooperman.com>
2026-03-26 16:32:23 -0400

    Updated and optimized site contents.

--- StaticWebApp/obj/Debug/net8.0/StaticWebApp.AssemblyInfo.cs
+++ StaticWebApp/obj/Debug/net8.0/StaticWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaticWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e810bdf99c09a2aebe4afd705de8e67aabac7db")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaticWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaticWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
